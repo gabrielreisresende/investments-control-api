b73bfe4a8b4aeac20d634793e7ecbcfd0622f871
Gabriel Resende <gabriellrr2005@gmail.com>
2024-07-01 12:16:09 -0300

    Project created

---
+++ src/main/java/com/resendegabriel/investmentscontrolapi/InvestmentsControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.resendegabriel.investmentscontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentsControlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestmentsControlApiApplication.class, args);
+    }
+
+}
