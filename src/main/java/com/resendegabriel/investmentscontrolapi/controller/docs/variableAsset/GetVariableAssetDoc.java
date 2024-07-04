83e412dd6f0ef514875736d0495321a06c75263c
Gabriel Resende <gabriellrr2005@gmail.com>
2024-07-04 20:35:18 -0300

    wallet-assets/docs: add swagger docs to variable assets endpoints

---
+++ src/main/java/com/resendegabriel/investmentscontrolapi/controller/docs/variableAsset/GetVariableAssetDoc.java
@@ -0,0 +1,50 @@
+package com.resendegabriel.investmentscontrolapi.controller.docs.variableAsset;
+
+import com.resendegabriel.investmentscontrolapi.exception.StandardError;
+import com.resendegabriel.investmentscontrolapi.model.dto.StockResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Operation(
+        summary = "Busca um ativo de renda variável",
+        description = "Busca um ativo de renda variável pelo seu id")
+@ApiResponses(value = {
+        @ApiResponse(
+                responseCode = "200",
+                description = "Retorna os dados de um ativo de renda variável",
+                content = @Content(
+                        mediaType = "application/json",
+                        array = @ArraySchema(schema = @Schema(implementation = StockResponse.class)))),
+        @ApiResponse(
+                responseCode = "400",
+                description = "Requisição Inválida",
+                content = @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = StandardError.class))),
+        @ApiResponse(
+                responseCode = "404",
+                description = "Recurso não encontrado",
+                content = @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = StandardError.class))),
+        @ApiResponse(
+                responseCode = "500",
+                description = "Erro no servidor",
+                content = @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = StandardError.class)))
+})
+public @interface GetVariableAssetDoc {
+}
